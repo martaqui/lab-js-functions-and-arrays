5ad4f431d1ef2c6ce72853d2d7f41ae82df8ca96
martaqui <martaqui133@gmail.com>
2024-10-18 17:14:28 +0200

    solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,14 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(smallerNum, biggerNum) {
+    if (smallerNum <= biggerNum) {
+        return biggerNum;
+    }
+
+    if (biggerNum < smallerNum) {
+        return smallerNum;
+    }
+
+}
 
 
 
@@ -7,28 +16,70 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(wordsArray) {
 
+    if (wordsArray.length === 0) {
+        return null;
+    }
 
+    const longestWordFound = ``;
+
+    for (let i = 0; i < wordsArray.length; i++) {
+
+        const eachWord = wordsArray[i];
+
+        if (eachWord.length === longestWordFound.length) {
+            longestWordFound = eachWord
+        }
+
+    }
+    return longestWordFound;
+}
 
 
 // Iteration 3 | Sum Numbers
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(numbersArray) {
+    let suma = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+        suma += numbersArray[i];
+    }
+    return suma;
+}
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(numbersArray) {
+    let suma = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+        suma += numbersArray[i];
+    }
+    return suma;
+}
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordsArray, searchWord) {
+
+    if (wordsArray.length === 0) {
+        return null;
+    }
+
+    let exsist = false;
+
+    for (let i = 0; i < wordsArray.length; i++) {
+        const eachWord = wordsArray[i];
+
+    } if (eachWord === searchWord) {
+        exsist = true;
+    }
+
+    return exist;
+
+}
